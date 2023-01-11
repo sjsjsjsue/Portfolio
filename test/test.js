73e0cc9fea184e24ce15903f822cb0833720a337
sjsjsjsue <esther106@naver.com>
2023-01-11 13:15:16 +0900

    test : upload test

---
+++ test/test.js
@@ -0,0 +1,15 @@
+// 이름 글자 나오기
+const content = "Hello, I'm Sue.";
+const text = document.querySelector('#main_top_write_bottom');
+let i = 0;
+
+function typing() {
+  let txt = content[i++];
+  text.innerHTML += txt;
+  if (i > content.length) {
+    text.textContent = '';
+    i = 0;
+  }
+}
+
+setInterval(typing, 300);
